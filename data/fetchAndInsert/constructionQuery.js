8d62f3461407aecdc2e4b9d364314dd662af6a82
Kashyap <kashyapvenattu@gmail.com>
2024-05-31 14:48:17 -0600

    update db string to cloud

--- data/fetchAndInsert/constructionQuery.js
+++ data/fetchAndInsert/constructionQuery.js
@@ -2,19 +2,21 @@ import mongoose from "mongoose";
 
 var Schema = mongoose.Schema;
 var constructionSchema = new Schema({
-    construction_info: {type: String, unique: true},
-    description: String,
-    start_dt: String,
-    end_dt: String,
-    longitude: String,
-    latitude: String,
-})
+  construction_info: { type: String, unique: true },
+  description: String,
+  start_dt: String,
+  end_dt: String,
+  longitude: String,
+  latitude: String,
+});
 
-var Construction = mongoose.model('Construction', constructionSchema);
+var Construction = mongoose.model("Construction", constructionSchema);
 
-await mongoose.connect('mongodb://localhost:27017/test')
-async function constructionQuery(){
-    return await Construction.find()
+await mongoose.connect(
+  "mongodb+srv://iridescentstormuser:k0GkmACeJvVh84Q0@cluster01.tw6gwdx.mongodb.net/?retryWrites=true&w=majority&appName=cluster01"
+);
+async function constructionQuery() {
+  return await Construction.find();
 }
 
 // // Move the below out to the frontend or controller to pass back only what is needed
@@ -28,4 +30,4 @@ async function constructionQuery(){
 // console.log(constuctionDetourArray)
 
 // mongoose.disconnect()
-export default constructionQuery
+export default constructionQuery;
