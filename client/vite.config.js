9d9f1525d57709e8a5e2eb319f0864301bb184fd
Kashyap <kashyapvenattu@gmail.com>
2024-06-01 00:20:09 -0600

    final updates - 13

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       "/api": {
         target: "https://c12-p2-team3.onrender.com",
         changeOrigin: true,
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
